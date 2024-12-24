a97f578a9da43ec614367e8049ad3c4dd8f035af
Bence Hervay <hervay.bence@gmail.com>
2024-12-24 23:11:30 +0000

    Linking Eigen and OpenCV

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Prince Rupert's Cube
+
+## Setup
+```bash
+sudo apt update
+sudo apt install libeigen3-dev
+sudo apt install libopencv-dev
+```
--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <Eigen/Dense>
+#include <opencv2/opencv.hpp>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    Eigen::Matrix2f matrix;
+    matrix << 1, 2, 3, 4;
+    std::cout << matrix << std::endl;
+
+    cv::Mat image = cv::Mat::zeros(400, 400, CV_8UC3);
+    cv::Point center = cv::Point(200, 200);
+    cv::Scalar color = cv::Scalar(0, 255, 0);
+    cv::circle(image, center, 100, color, 2);
+    cv::imwrite("image.png", image);
+
     return 0;
 }
