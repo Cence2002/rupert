59f9b71f92eeb9b1ff80067c6216a234cf3514e7
Bence Hervay <hervay.bence@gmail.com>
2025-01-08 20:11:12 +0000

    Queue3 + Queue2 iterations combined

--- main.cpp
+++ main.cpp
@@ -9,10 +9,9 @@
 std::vector<Vector3d> polyhedron = Polyhedron::rhombicosidodecahedron();
 
 Plot plot_main("main", 1000, 1000, {-1.5, 1.5}, {-1.5, 1.5});
-Plot plot_boxes("boxes", 1000, 1000, {0, 1}, {0, 1});
+Plot plot_queue("queue", 1000, 1000, {0, 1}, {0, 1});
 
-std::vector<Vector2d> hole;
-Queue2 boxes;
+Queue3 queue3(2, 2, 2);
 
 void test() {
     test_angle();
@@ -22,87 +21,22 @@ void test() {
 
 void setup() {
     plot_main.clear();
-    plot_boxes.clear();
+    plot_queue.clear();
     plot_main.move({100, 100});
-    plot_boxes.move({1150, 100});
-    plot_boxes.show();
-    plot_main.show();
-
-    Interval hole_alpha(0);
-    Interval hole_theta(0);
-    Interval hole_phi(-0.01, 0.01);
-    std::vector<Vector2d> hole_all;
-    for(const Interval &alpha: divide(hole_alpha, 10)) {
-        for(const Interval &theta: divide(hole_theta, 10)) {
-            for(const Interval &phi: divide(hole_phi, 10)) {
-                for(const Vector3d &vertex: polyhedron) {
-                    const Vector2I v_project = Vector3I(vertex.x, vertex.y, vertex.z).project(theta, phi);
-                    const auto [x, y] = v_project.rotate(alpha);
-                    hole_all.emplace_back(lower(x), lower(y));
-                    hole_all.emplace_back(lower(x), upper(y));
-                    hole_all.emplace_back(upper(x), lower(y));
-                    hole_all.emplace_back(upper(x), upper(y));
-                }
-            }
-        }
-    }
-    hole = convex_hull(hole_all);
-    std::ranges::sort(hole, [](const Vector2d &v0, const Vector2d &v1) {
-        return v0.angle() < v1.angle();
-    });
-}
-
-void process() {
-    if(boxes.empty()) {
-        return;
-    }
-    const Box2 box = boxes.fetch();
-    for(const Vector3d &vertex: polyhedron) {
-        if(!box_intersects_polygon(vertex, box, hole)) {
-            return;
-        }
-    }
-    boxes.push_sub_boxes(box);
-}
-
-void plot() {
-    plot_main.clear();
-    plot_boxes.clear();
-
-    plot_main.line(Vector2d(-10, 0), Vector2d(10, 0), Colors::GRAY);
-    plot_main.line(Vector2d(0, -10), Vector2d(0, 10), Colors::GRAY);
-    plot_main.circle(Vector2d(0, 0), 1, Colors::GRAY);
-    plot_main.polygon(hole, Colors::CYAN);
-
-    for(const Box2 &box: boxes.vector()) {
-        if(std::max(width(box.theta), width(box.phi)) < 0.01) {
-            plot_boxes.circle(box.normalized().center(), 0.1, gradient(box) / 2);
-        }
-    }
-    for(const Box2 &box: boxes.vector()) {
-        plot_boxes.filled_box(box.normalized(), gradient(box) / 2);
-        plot_boxes.box(box.normalized(), gradient(box));
-    }
-    plot_boxes.show();
-    plot_main.show();
+    plot_queue.move({1150, 100});
+    // plot_queue.show();
+    // plot_main.show();
 }
 
 void draw() {
-    while(Plot::step(1)) {
-        for(int t = 0; t < 100; t++) {
-            process();
-        }
-        plot();
-        std::cout << boxes.size() << std::endl;
-        if(boxes.empty()) {
-            break;
-        }
+    while(!queue3.empty()) {
+        queue3.process(polyhedron);
     }
 }
 
 void exit() {
     plot_main.save("../outputs/main.png");
-    plot_boxes.save("../outputs/boxes.png");
+    plot_queue.save("../outputs/queue.png");
     cv::destroyAllWindows();
 }
 
--- src/queue.hpp
+++ src/queue.hpp
@@ -30,13 +30,26 @@ struct Queue2 : std::queue<Box2> {
             push(sub_box);
         }
     }
+
+    void process(const std::vector<Vector3d> &polyhedron, const std::vector<Vector2d> &hole) {
+        if(empty()) {
+            return;
+        }
+        const Box2 box = fetch();
+        for(const Vector3d &vertex: polyhedron) {
+            if(!box_intersects_polygon(vertex, box, hole)) {
+                return;
+            }
+        }
+        push_sub_boxes(box);
+    }
 };
 
 struct Queue3 : std::queue<Box3> {
     explicit Queue3(const int theta_parts = 1, const int phi_parts = 1, const int alpha_parts = 1) {
-        for(const Interval &theta: divide(Box3::theta_interval, theta_parts)) {
-            for(const Interval &phi: divide(Box3::phi_interval, phi_parts)) {
-                for(const Interval &alpha: divide(Box3::alpha_interval, alpha_parts)) {
+        for(const Interval &theta: divide(Box3::theta_interval / 4.0 + 1.0, theta_parts)) {
+            for(const Interval &phi: divide(Box3::phi_interval / 4.0 + 1.0, phi_parts)) {
+                for(const Interval &alpha: divide(Box3::alpha_interval / 4.0 + 1.0, alpha_parts)) {
                     emplace(theta, phi, alpha);
                 }
             }
@@ -60,6 +73,44 @@ struct Queue3 : std::queue<Box3> {
     void push_sub_boxes(const Box3 &box) {
         for(const Box3 &sub_box: box.sub_boxes()) {
             push(sub_box);
+        }
+    }
+
+    void process(const std::vector<Vector3d> &polyhedron, const int resolution = 10, const int iterations = 10000) {
+        if(empty()) {
+            return;
+        }
+        const Box3 box = fetch();
+        std::vector<Vector2d> hole_all;
+        for(const Interval &theta: divide(box.theta, resolution)) {
+            for(const Interval &phi: divide(box.phi, resolution)) {
+                for(const Interval &alpha: divide(box.alpha, resolution)) {
+                    for(const auto &[x, y, z]: polyhedron) {
+                        const auto [x_interval, y_interval] = Vector3I(x, y, z).project(theta, phi).rotate(alpha);
+                        hole_all.emplace_back(lower(x_interval), lower(y_interval));
+                        hole_all.emplace_back(lower(x_interval), upper(y_interval));
+                        hole_all.emplace_back(upper(x_interval), lower(y_interval));
+                        hole_all.emplace_back(upper(x_interval), upper(y_interval));
+                    }
+                }
+            }
+        }
+        std::vector<Vector2d> hole = convex_hull(hole_all);
+        std::ranges::sort(hole, [](const Vector2d &vertex_0, const Vector2d &vertex_1) {
+            return vertex_0.angle() < vertex_1.angle();
+        });
+
+        Queue2 queue2;
+        for(int t = 0; t < iterations; t++) {
+            queue2.process(polyhedron, hole);
+        }
+        if(!queue2.empty()) {
+            push_sub_boxes(box);
+        }
+        if(!queue2.empty()) {
+            std::cout << "Divided Box3 " << box << std::endl;
+        } else {
+            std::cout << "Empty Box3 " << box << std::endl;
         }
     }
 };
