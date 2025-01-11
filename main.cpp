80f29fa4e683fe4473a396dd611f8dcc8c34bce9
Bence Hervay <hervay.bence@gmail.com>
2025-01-11 23:39:35 +0000

    Queue3 thread-safe queue, Queue2 sequential

--- main.cpp
+++ main.cpp
@@ -6,56 +6,26 @@
 #include "src/test.hpp"
 #include <iostream>
 
-std::vector<Vector3d> polyhedron = Polyhedron::rhombicosidodecahedron();
-
-// Plot plot_main("main", 1000, 1000, {-1.5, 1.5}, {-1.5, 1.5});
-// Plot plot_queue("queue", 1000, 1000, {0, 1}, {0, 1});
-
-Queue3 queue3;
-
-void test() {
-    test_angle();
-
-    std::cout << "All tests passed" << std::endl;
-}
-
-void setup() {
-    // plot_main.clear();
-    // plot_queue.clear();
-    // plot_main.move({100, 100});
-    // plot_queue.move({1150, 100});
-    // plot_queue.show();
-    // plot_main.show();
-}
-
-void draw() {
-    while(!queue3.empty()) {
-        queue3.process(polyhedron, 5, 20000, 20, 20);
-    }
-}
-
-void exit() {
-    // plot_main.save("../outputs/main.png");
-    // plot_queue.save("../outputs/queue.png");
-    // cv::destroyAllWindows();
-}
-
 int main() {
-    test();
-    setup();
-    draw();
-    exit();
+    std::vector<Vector3d> polyhedron = Polyhedron::rhombicosidodecahedron();
+
+    Queue3 queue3(10, 10, 10);
+    auto start = std::chrono::high_resolution_clock::now();
+    std::vector<std::thread> threads;
+    const int thread_count = 10;
+    for(int i = 0; i < thread_count; i++) {
+        threads.emplace_back([&] {
+            for(int j = 0; j < 20 / thread_count; j++) {
+                queue3.process(polyhedron, 5, 20000, 100);
+            }
+        });
+    }
+    for(auto &thread: threads) {
+        thread.join();
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    std::cout << "Elapsed time: " << std::chrono::duration_cast<std::chrono::seconds>(end - start).count() << "s" << std::endl;
+    // Takes approximately 10s
+
     return 0;
 }
-
-// #include <opencv2/viz.hpp>
-// #include <opencv2/viz/widgets.hpp>
-// #include <opencv2/core.hpp>"
-//
-// int main() {
-//     cv::viz::Viz3d window("3D Scene");
-//     window.showWidget("Line", cv::viz::WLine(cv::Point3d(0, 0, 0), cv::Point3d(1, 1, 1), cv::viz::Colors::red()));
-//     window.showWidget("CoordinateSystem", cv::viz::WCoordinateSystem(1.0));
-//     window.spin();
-//     return 0;
-// }
--- src/queue.hpp
+++ src/queue.hpp
@@ -2,74 +2,75 @@
 
 #include "interval.hpp"
 #include <optional>
+#include <random>
 #include <thread>
 #include <boost/lockfree/queue.hpp>
 
-class Queue2 {
-    boost::lockfree::queue<TrivialBox2> queue;
-    std::atomic<int> size = 0;
+template<typename T>
+class Queue {
+    std::queue<T> queue;
+    std::mutex mutex;
 
 public:
-    explicit Queue2(const int size, const int theta_parts = 1, const int phi_parts = 1) : queue(size) {
+    void push(const T &value) {
+        std::lock_guard<std::mutex> lock(mutex);
+        queue.push(value);
+    }
+
+    std::optional<T> pop() {
+        std::lock_guard<std::mutex> lock(mutex);
+        if(queue.empty()) {
+            return std::nullopt;
+        }
+        const T value = queue.front();
+        queue.pop();
+        return std::make_optional(value);
+    }
+
+    std::vector<T> vector() {
+        std::lock_guard<std::mutex> lock(mutex);
+        std::vector<T> values;
+        for(const T &value: queue.c) {
+            values.push_back(value);
+        }
+        return values;
+    }
+};
+
+class Queue2 {
+    std::queue<Box2> queue;
+
+public:
+    explicit Queue2(const int theta_parts = 1, const int phi_parts = 1) {
         for(const Interval &theta: divide(Box2::theta_interval, theta_parts)) {
             for(const Interval &phi: divide(Box2::phi_interval, phi_parts)) {
-                const Box2 box(theta, phi);
-                push(box.trivial());
+                queue.push({theta, phi});
             }
         }
     }
 
     bool empty() const {
-        return size == 0;
+        return queue.empty();
     }
 
-    std::optional<TrivialBox2> pop(const bool decrement = true) {
-        TrivialBox2 box;
-        while(!queue.pop(box)) {
-            if(empty()) {
-                return std::nullopt;
-            }
+    void process(const std::vector<Vector3d> &polyhedron, const std::vector<Vector2d> &hole) {
+        if(queue.empty()) {
+            return;
         }
-        if(decrement) {
-            --size;
-        }
-        return box;
-    }
-
-    void push(const TrivialBox2 &box) {
-        ++size;
-        while(!queue.push(box)) {
-            std::cout << "Queue2 push failed" << std::endl;
-        }
-    }
-
-    void push_sub_boxes(const Box2 &box) {
-        for(const Box2 &sub_box: box.sub_boxes()) {
-            push(sub_box.trivial());
-        }
-    }
-
-    bool process(const std::vector<Vector3d> &polyhedron, const std::vector<Vector2d> &hole, const bool check_fit = false) {
-        if(empty()) {
-            return false;
-        }
-        const std::optional<TrivialBox2> optional_box = pop(false);
-        if(!optional_box.has_value()) {
-            return false;
-        }
-        const Box2 box(optional_box.value());
+        const Box2 box = queue.front();
+        queue.pop();
         for(const Vector3d &vertex: polyhedron) {
             if(!box_intersects_polygon(box, vertex, hole)) {
-                --size;
-                return false;
+                return;
             }
         }
-        push_sub_boxes(box);
-        --size;
-
-        if(!check_fit) {
-            return false;
+        for(const Box2 &sub_box: box.sub_boxes()) {
+            queue.push(sub_box);
         }
+    }
+
+    bool check(const std::vector<Vector3d> &polyhedron, const std::vector<Vector2d> &hole) const {
+        const Box2 box = queue.front();
         for(const Vector3d &vertex: polyhedron) {
             if(!point_inside_polygon(vertex.project(median(box.theta), median(box.phi)), hole)) {
                 return false;
@@ -80,40 +81,30 @@ public:
 };
 
 class Queue3 {
-    std::queue<Box3> queue;
+    Queue<Box3> queue;
 
 public:
     explicit Queue3(const int theta_parts = 1, const int phi_parts = 1, const int alpha_parts = 1) {
+        std::vector<Box3> boxes;
         for(const Interval &theta: divide(Box3::theta_interval, theta_parts)) {
             for(const Interval &phi: divide(Box3::phi_interval, phi_parts)) {
                 for(const Interval &alpha: divide(Box3::alpha_interval, alpha_parts)) {
-                    queue.push({theta, phi, alpha});
+                    boxes.push_back({theta, phi, alpha});
                 }
             }
         }
-    }
-
-    bool empty() const {
-        return queue.empty();
-    }
-
-    Box3 pop() {
-        const Box3 box = queue.front();
-        queue.pop();
-        return box;
-    }
-
-    void push_sub_boxes(const Box3 &box) {
-        for(const Box3 &sub_box: box.sub_boxes()) {
-            queue.push(sub_box);
+        std::shuffle(boxes.begin(), boxes.end(), std::mt19937(std::random_device()()));
+        for(const Box3 &box: boxes) {
+            queue.push(box);
         }
     }
 
-    void process(const std::vector<Vector3d> &polyhedron, const int resolution = 2, const int iterations = 10000, const int frequency = 10, const int thread_count = 10) {
-        if(empty()) {
+    void process(const std::vector<Vector3d> &polyhedron, const int resolution = 2, const int iterations = 10000, const int frequency = 10) {
+        const std::optional<Box3> optional_box = queue.pop();
+        if(!optional_box.has_value()) {
             return;
         }
-        const Box3 box = pop();
+        const Box3 &box = optional_box.value();
         std::vector<Vector2d> hole_all;
         for(const Interval &theta: divide(box.theta, resolution)) {
             for(const Interval &phi: divide(box.phi, resolution)) {
@@ -133,25 +124,20 @@ public:
             return vertex_0.angle() < vertex_1.angle();
         });
 
-        Queue2 queue2(iterations * 8);
-
-        std::vector<std::thread> threads;
-        bool done = false;
-        for(int i = 0; i < thread_count; i++) {
-            threads.emplace_back([&] {
-                for(int t = 0; !done && t < iterations / thread_count + 1; t++) {
-                    if(queue2.process(polyhedron, hole, t % thread_count == 0)) {
-                        done = true;
-                    }
+        Queue2 queue2;
+        for(int i = 0; i < iterations; i++) {
+            if(iterations % frequency == 0) {
+                if(queue2.check(polyhedron, hole)) {
+                    // break;
                 }
-            });
-        }
-        for(std::thread &thread: threads) {
-            thread.join();
+            }
+            queue2.process(polyhedron, hole);
         }
 
         if(!queue2.empty()) {
-            push_sub_boxes(box);
+            for(const Box3 &sub_box: box.sub_boxes()) {
+                queue.push(sub_box);
+            }
             std::cout << "Divided Box3 " << box << std::endl;
         } else {
             std::cout << "Empty Box3 " << box << " volume " << width(box.theta) * width(box.phi) * width(box.alpha) << std::endl;
