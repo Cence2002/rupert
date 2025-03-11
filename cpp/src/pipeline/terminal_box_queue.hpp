4ba6c7d93a5893b0e862154ffc820666348c1aaa
Bence Hervay <hervay.bence@gmail.com>
2025-03-11 00:13:04 +0000

    Implemented TerminalBoxQueue

--- cpp/src/pipeline/terminal_box_queue.hpp
+++ cpp/src/pipeline/terminal_box_queue.hpp
@@ -11,9 +11,27 @@ private:
     size_t size_;
 
 public:
-    size_t size() const;
+    TerminalBoxQueue() : queue_(), mutex_(), size_(0) {}
 
-    void push(const TerminalBox& terminal_box);
+    size_t size() const {
+        return size_;
+    }
 
-    std::vector<TerminalBox> flush();
+    void push(const TerminalBox& terminal_box) {
+        std::lock_guard<std::mutex> lock(mutex_);
+        queue_.push(terminal_box);
+        size_ += terminal_box.size();
+    }
+
+    std::vector<TerminalBox> flush() {
+        std::lock_guard<std::mutex> lock(mutex_);
+        std::vector<TerminalBox> terminal_boxes;
+        terminal_boxes.reserve(queue_.size());
+        while(!queue_.empty()) {
+            terminal_boxes.push_back(queue_.front());
+            queue_.pop();
+        }
+        size_ = 0;
+        return terminal_boxes;
+    }
 };
