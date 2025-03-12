e9aa0fec17ceb2b0cd2a15536ec3d7335b009435
Bence Hervay <hervay.bence@gmail.com>
2025-03-12 20:41:06 +0000

    Header test script

--- cpp/scripts/build.sh
+++ cpp/scripts/build.sh
@@ -5,6 +5,7 @@ set -e
 cd "$(dirname "${BASH_SOURCE[0]}")/../"
 
 BUILD_DIR="build"
+
 rm -rf $BUILD_DIR
 mkdir $BUILD_DIR
 cmake -B $BUILD_DIR -S .
---
+++ cpp/scripts/headers_test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")/../"
+
+SOURCE_DIR="src"
+
+CPP_FILE="temp.cpp"
+
+for HEADER_PATH in $(find $SOURCE_DIR -name '*.hpp'); do
+  echo "#include \"$HEADER_PATH\"" > $CPP_FILE
+  g++ -std=c++20 -fsyntax-only -w -I $SOURCE_DIR -c $CPP_FILE
+  rm $CPP_FILE
+  echo "Compiled: $HEADER_PATH"
+done
