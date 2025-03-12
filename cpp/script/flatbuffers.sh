643021838f2ad39c933504a4e1bc5e200cf3e8d6
Bence Hervay <hervay.bence@gmail.com>
2025-03-12 19:05:04 +0000

    Moving scripts in CPP

--- cpp/script/flatbuffers.sh
+++ cpp/script/flatbuffers.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+set -e
+
+cd "$(dirname "${BASH_SOURCE[0]}")/../"
+
 flatc --cpp -o src/flatbuffers/ src/flatbuffers.fbs
 
 flatc --ts -o ../web/src/lib/flatbuffers/ src/flatbuffers.fbs
