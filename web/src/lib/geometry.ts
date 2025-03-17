d0e960cc2e4a961089fc7a254b839e85d7f1c370
Bence Hervay <hervay.bence@gmail.com>
2025-03-17 21:21:49 +0000

    Gift wrapping convex hull

--- web/src/lib/geometry.ts
+++ web/src/lib/geometry.ts
@@ -2,29 +2,25 @@ import {Vector2} from "three";
 
 
 export function convexHull(points: Vector2[]): Vector2[] {
-    points.sort((a, b) => a.x - b.x || a.y - b.y);
+    if (points.length < 3) return points;
 
-    const cross = (o: Vector2, a: Vector2, b: Vector2) =>
-        (a.x - o.x) * (b.y - o.y) - (a.y - o.y) * (b.x - o.x);
+    let hull: Vector2[] = [];
+    let leftmost = points.reduce((left, p) => (p.x < left.x ? p : left), points[0]);
+    let current = leftmost;
 
-    let lower: Vector2[] = [];
-    for (let p of points) {
-        while (lower.length >= 2 && cross(lower[lower.length - 2], lower[lower.length - 1], p) <= 0) {
-            lower.pop();
+    do {
+        hull.push(current);
+        let next = points[0] === current ? points[1] : points[0];
+
+        for (let i = 0; i < points.length; i++) {
+            let cross = (next.x - current.x) * (points[i].y - current.y) - (next.y - current.y) * (points[i].x - current.x);
+            if (cross < 0 || (cross === 0 && current.distanceTo(points[i]) > current.distanceTo(next))) {
+                next = points[i];
+            }
         }
-        lower.push(p);
-    }
 
-    let upper: Vector2[] = [];
-    for (let i = points.length - 1; i >= 0; i--) {
-        let p = points[i];
-        while (upper.length >= 2 && cross(upper[upper.length - 2], upper[upper.length - 1], p) <= 0) {
-            upper.pop();
-        }
-        upper.push(p);
-    }
+        current = next;
+    } while (current !== leftmost);
 
-    upper.pop();
-    lower.pop();
-    return lower.concat(upper);
+    return hull;
 }
