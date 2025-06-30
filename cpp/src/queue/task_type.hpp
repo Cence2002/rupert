9f94820757048cc6cb4a80e7d7970246315c063a
Bence Hervay <hervay.bence@gmail.com>
2025-07-01 00:15:51 +0100

    Finally get rid of BoxProcessor

--- cpp/src/queue/task_type.hpp
+++ cpp/src/queue/task_type.hpp
@@ -8,7 +8,9 @@ concept TaskType =
     std::copy_constructible<Task> &&
     std::destructible<Task> &&
     std::constructible_from<std::optional<Task>, Task> &&
+
     requires(std::queue<Task>& queue, const Task& task) {
         { queue.push(task) } -> std::same_as<void>;
         { queue.front() } -> std::convertible_to<Task>;
+        { queue.pop() } -> std::same_as<void>;
     };
