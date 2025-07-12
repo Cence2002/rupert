3a23b454821c5d09813181a93509351f97d7bc50
Bence Hervay <hervay.bence@gmail.com>
2025-07-12 10:29:38 +0100

    Update interval print formatting

--- cpp/src/interval/intervals.hpp
+++ cpp/src/interval/intervals.hpp
@@ -12,7 +12,7 @@ enum class PrintMode {
 };
 
 inline PrintMode print_mode = PrintMode::mid_and_rad;
-inline std::streamsize print_precision = 4;
+inline std::streamsize print_precision = 3;
 
 inline std::string to_string(const double value) {
     std::ostringstream stream;
@@ -21,17 +21,25 @@ inline std::string to_string(const double value) {
     return stream.str();
 }
 
+inline std::string to_string(const double value, const std::ios_base::fmtflags format) {
+    std::ostringstream stream;
+    stream.precision(print_precision);
+    stream.setf(format, std::ios_base::floatfield);
+    stream << value;
+    return stream.str();
+}
+
 template<IntervalType Interval>
 std::ostream& operator<<(std::ostream& ostream, const Interval& interval) {
     switch(print_mode) {
         case PrintMode::min_and_max: {
-            return ostream << to_string(interval.min().to_float()) << ":" << to_string(interval.max().to_float());
+            return ostream << to_string(interval.min().to_float(), std::ios_base::fixed) << ":" << to_string(interval.max().to_float(), std::ios_base::fixed);
         }
         case PrintMode::mid_and_rad: {
-            return ostream << to_string(interval.mid().to_float()) << "~" << to_string(interval.rad().to_float());
+            return ostream << to_string(interval.mid().to_float(), std::ios_base::fixed) << "~" << to_string(interval.rad().to_float(), std::ios_base::scientific);
         }
         case PrintMode::mid_only: {
-            return ostream << to_string(interval.mid().to_float());
+            return ostream << to_string(interval.mid().to_float(), std::ios_base::fixed);
         }
         default: throw std::invalid_argument("Unknown PrintMode");
     }
